5fd6d4a3bc62bab7f3ebc20f63df9a0b23594b87
karlekvist <adrian.carl.pettersson@gmail.com>
2020-08-08 14:58:03 +0200

    Multiplying two values. No validation

---
+++ chapter 2/excercise2/ComputeGrossPay.py
@@ -0,0 +1,8 @@
+# Write a program to prompt the user for hours and rate per hour to compute gross pay
+
+print("Enter Hours: ")
+hours = float(input())
+print("Enter Rate: ")
+rate = float(input())
+print(round(hours * rate))
+
